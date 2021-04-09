42d8baa4f08a72d83540f6089c59c7bb87bfc613
mat <justmat@mykolab.com>
2021-04-09 09:53:15 -0400

    param grouping. adjust fade time range

--- silos.lua
+++ silos.lua
@@ -187,7 +187,6 @@ function init()
     params:set_action(i .. "send", function(value) engine.send(i, value) end)
   end
 
-  params:add_separator()
    params:add_group("fx", 13)
   -- effect controls
   -- delay time
@@ -236,7 +235,7 @@ function init()
   params:set_action("grid_mode", function(value) silos.grid_mode = value end)
 
   for i = 1, 4 do
-    params:add_control(i .."fade_time", i .. " gate fade time", controlspec.new(0, 15, "lin", 0.1, 0, "secs"))
+    params:add_control(i .."fade_time", i .. " gate fade time", controlspec.new(0, 30, "lin", 0.1, 0, "secs"))
     params:set_action(i .. "fade_time", function(value) engine.envscale(i, value) end)
   end
 
